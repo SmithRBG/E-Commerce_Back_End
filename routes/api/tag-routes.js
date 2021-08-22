81bd956f3c7e5a182d007b14a553d8e59e60739c
SmithRBG <smit36000@gmail.com>
2021-08-22 16:41:05 -0400

    routes update

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,19 +6,32 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-  try {
+ /*  try {
     const tagData = await tag.findAll({
       include: [{ model: Tag }, { model: ProductTag }, {model: Product},],
     });
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
-  }
+  } */
+  Tag.findAll ({
+    include:[ProductTag]
+  })
+  .then ((tags) => res.json(tags))
+  .catch ((err) => res.json(err))
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne ({
+    where: {
+      id:req.params.id
+    },
+    include:[ProductTag]
+  })
+  .then ((tags)=> res.json(tags))
+  .catch ((err)=> res.json(err))
 });
 
 router.post('/', (req, res) => {
